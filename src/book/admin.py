e7a26bba9ad4b6e8b2f45c2a0207dfc1b0d452b9
harshita97 <hrshita97@gmail.com>
2017-02-26 18:27:20 +0530

    addedtupple

--- src/book/admin.py
+++ src/book/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 # Register your models here.
-from .models import Book, LendPeriods, Isbn
+from .models import Book, LendPeriods, Isbn, UserProfile
 
 admin.site.register(Book)
 admin.site.register(LendPeriods)
 admin.site.register(Isbn)
+admin.site.register(UserProfile)
